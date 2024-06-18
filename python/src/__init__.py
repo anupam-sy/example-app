c8150db8d6bbe36874e56ceb811ea83bb80a9501
Anupam <43881706+anupam-sy@users.noreply.github.com>
2024-06-18 17:58:52 +0530

    feat: update the dockerfile and code (#2)
    * feat: folders restructured/renamed
    
    * added the dockeringore file
    
    * feat: docker file updated
    
    * added doc strings and comments in code

--- python/src/__init__.py
+++ python/src/__init__.py
@@ -1,1 +1,10 @@
-from .main import app
+"""
+src: This package contains modules for various functionalities:
+
+- main: Contains flask code to run a sample website.
+- validlogin: Implements login validity logic to login to application.
+"""
+
+# Package version
+__all__ = ['main', 'validlogin']
+__version__ = '1.0.0'
